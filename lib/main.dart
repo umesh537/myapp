4e28f617736c59a54fde5af0c048874760763c45
Umesh <umesh.surya69@gmail.com>
2022-01-20 18:37:29 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+          child: Text("Lets get Started"),
+        ),
+      ),
+    ),
+    );
+  }
+}
