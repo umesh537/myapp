2837f7436cebb752171bd8411736372fb29cedd4
Umesh <umesh.surya69@gmail.com>
2022-02-11 20:16:06 +0530

    day1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/home.dart';
+import 'package:myapp/pages/home.dart';
 
 void main() {
   runApp(myapp());
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,4 +15,4 @@ class HomePage extends StatelessWidget {
       drawer: Drawer(),
     );
   }
-}
+} 
